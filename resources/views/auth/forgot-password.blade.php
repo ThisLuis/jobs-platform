ea698866946edb1c8ef0a755f50d8751af600620
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-01-07 18:30:52 -0600

    Se termino de personalizar los estilos de las vistas de crear cuenta. iniciar sesion y olivde contraseña

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Olvidaste tu contraseña? Introduce tu email de registro y te enviaremos un email para cambiar tu contraseña') }}
     </div>
 
     <!-- Session Status -->
@@ -15,11 +15,26 @@
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+        
+        <div class="flex items-center justify-between my-5">
+            <x-link
+                :href="route('login')"
+            >
+                Iniciar sesión
+            </x-link>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+            <x-link
+                :href="route('register')"
+            >
+                Crear cuenta
+            </x-link>
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Enviar instrucciones') }}
+        </x-primary-button>
+       
+            
+        
     </form>
 </x-guest-layout>
